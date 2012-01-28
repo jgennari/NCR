4ed636ba1d38f5f7ce526d8f51e34f203408aefe
Joe Gennari <jgennari@gmail.com>
2012-01-28 13:05:35 -0500

    third add

---
+++ NCR/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("No Click Rip")]
+[assembly: AssemblyDescription("An automatic DVD ripping tool.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pro Team Agency, Inc.")]
+[assembly: AssemblyProduct("NCR")]
+[assembly: AssemblyCopyright("Copyright © Pro Team Agency, Inc. 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("09dae63f-933d-4e30-a71e-f6f03ab44252")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.2.5")]
+[assembly: AssemblyFileVersion("0.2.5")]
